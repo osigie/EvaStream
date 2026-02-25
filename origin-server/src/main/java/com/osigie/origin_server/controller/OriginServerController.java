a54db4bc8766f6a4be97db5d7553e4129cd2bec3
osigie <kenosagie88@gmail.com>
2026-02-26 00:05:49 +0100

    create saving to disc and saving metadata

--- origin-server/src/main/java/com/osigie/origin_server/controller/OriginServerController.java
+++ origin-server/src/main/java/com/osigie/origin_server/controller/OriginServerController.java
@@ -1,12 +1,10 @@
 package com.osigie.origin_server.controller;
+
 import com.osigie.origin_server.model.Chunk;
 import com.osigie.origin_server.service.OriginServerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -21,4 +19,5 @@ public class OriginServerController {
     public ResponseEntity<Chunk> getChunk(@RequestParam(name = "songId") Long songId, @RequestParam(name = "chunkId") Long chunkId) {
         return new ResponseEntity<>(this.originServerService.fetchChunk(songId, chunkId), HttpStatus.OK);
     }
+
 }
