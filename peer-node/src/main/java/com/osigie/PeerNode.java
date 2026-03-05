d86b5cca09a04e4cf08c11c4e73f9bae8bff0400
osigie <kenosagie88@gmail.com>
2026-03-05 11:48:24 +0100

    add network manual test

--- peer-node/src/main/java/com/osigie/PeerNode.java
+++ peer-node/src/main/java/com/osigie/PeerNode.java
@@ -3,6 +3,10 @@ package com.osigie;
 
 import com.osigie.domain.ChunkAcquired;
 import com.osigie.domain.PeerInfo;
+import com.osigie.networking.PeerClient;
+import com.osigie.networking.PeerServer;
+import com.osigie.service.ChunkStore;
+import com.osigie.service.MetadataClient;
 import com.osigie.service.OriginClient;
 import utils.HttpClientInstance;
 import com.osigie.service.TrackerClient;
@@ -10,8 +14,11 @@ import com.osigie.service.TrackerClient;
 import java.util.UUID;
 
 public class PeerNode {
-    public static void main(String[] args) {
 
+    //    TODO: pass through env or variable
+    private static final String peerId = "peer-001";
+
+    public static void main(String[] args) throws InterruptedException {
         //Tracker client
         TrackerClient trackerClient = new TrackerClient(HttpClientInstance.getInstance());
 
@@ -23,7 +30,7 @@ public class PeerNode {
                 }).join();
 
 
-        PeerInfo peerInfo = new PeerInfo("peer-001", "localhost", 8080);
+        PeerInfo peerInfo = new PeerInfo(peerId, "localhost", 8080);
 
         trackerClient.register(peerInfo)
                 .thenAccept(System.out::println)
@@ -65,7 +72,10 @@ public class PeerNode {
 //                    return null;
 //                }).join();
 
-        originClient.fetchChunk("e59af557-84c4-4042-97aa-bb9da8795427", "17761071-7c0f-4187-ba40-c21357578838")
+        String sSongId = "535ef1f1-8592-419f-8e4d-2fb85fcb264c";
+        String sChunkId = "7ebefc30-8d77-4295-9684-f1cd283197a4";
+
+        originClient.fetchChunk(sChunkId, sSongId)
                 .thenAccept(System.out::println)
                 .exceptionally(e -> {
                     System.out.println(e.getMessage());
@@ -73,7 +83,31 @@ public class PeerNode {
                 }).join();
 
 
+        MetadataClient metadataClient = new MetadataClient(HttpClientInstance.getInstance());
+
+        metadataClient.getMetadata(songId)
+                .thenAccept(System.out::println)
+                .exceptionally(e -> {
+                    System.out.println(e.getMessage());
+                    return null;
+                }).join();
+
+
+        ChunkStore chunkStore = new ChunkStore();
+        PeerServer peerServer = new PeerServer(9000, chunkStore, peerId);
+
+
+
+        peerServer.start();
+
+        PeerClient peerClient = new PeerClient();
+
+        peerClient.requestChunk("localhost", 9000, "535ef1f1-8592-419f-8e4d-2fb85fcb264c", "7ebefc30-8d77-4295-9684-f1cd283197a4", () -> {
+            System.out.println("Chunk received!");
+        });
+
         trackerClient.heartbeatRunner(peerInfo.peerId());
+
     }
 
 }
