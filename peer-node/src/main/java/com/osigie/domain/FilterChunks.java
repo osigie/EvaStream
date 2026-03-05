b6e95f11788ae8f76bd6b669c21c8804ee0f823a
osigie <kenosagie88@gmail.com>
2026-03-05 17:33:55 +0100

    feat(domain): add FilterChunks to move data

--- peer-node/src/main/java/com/osigie/domain/FilterChunks.java
+++ peer-node/src/main/java/com/osigie/domain/FilterChunks.java
@@ -1,4 +1,30 @@
 package com.osigie.domain;
 
+import java.util.List;
+import java.util.TreeSet;
+
 public class FilterChunks {
+    private List<SongChunkDto> chunksToFetch;
+    private TreeSet<SongChunkDto> localChunks;
+
+    public FilterChunks(List<SongChunkDto> chunksToFetch, TreeSet<SongChunkDto> localChunks) {
+        this.chunksToFetch = chunksToFetch;
+        this.localChunks = localChunks;
+    }
+
+    public List<SongChunkDto> getChunksToFetch() {
+        return chunksToFetch;
+    }
+
+    public void setChunksToFetch(List<SongChunkDto> chunksToFetch) {
+        this.chunksToFetch = chunksToFetch;
+    }
+
+    public TreeSet<SongChunkDto> getLocalChunks() {
+        return localChunks;
+    }
+
+    public void setLocalChunks(TreeSet<SongChunkDto> localChunks) {
+        this.localChunks = localChunks;
+    }
 }
