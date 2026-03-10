6fd42514b72d113f3f41e736c924b41eba041dca
osigie <kenosagie88@gmail.com>
2026-03-10 18:07:17 +0100

    feat(Scheduler): add chunkAcquired method to allow tracker service to keep track

--- peer-node/src/main/java/com/osigie/service/Scheduler.java
+++ peer-node/src/main/java/com/osigie/service/Scheduler.java
@@ -1,9 +1,6 @@
 package com.osigie.service;
 
-import com.osigie.domain.Chunk;
-import com.osigie.domain.FilterChunks;
-import com.osigie.domain.PeerInfo;
-import com.osigie.domain.SongChunkDto;
+import com.osigie.domain.*;
 import com.osigie.networking.PeerClient;
 import com.osigie.service.clients.OriginClient;
 import com.osigie.service.clients.TrackerClient;
@@ -16,17 +13,23 @@ public class Scheduler {
     private final OriginClient originClient;
     private final PeerClient peerClient;
     private final ChunkStore chunkStore;
+    private final String peerId;
+    private final int port;
 
-    public Scheduler(TrackerClient trackerClient, OriginClient originClient, PeerClient peerClient, ChunkStore chunkStore) {
+    public Scheduler(TrackerClient trackerClient, OriginClient originClient, PeerClient peerClient, ChunkStore chunkStore, String peerId, int port) {
         this.trackerClient = trackerClient;
         this.originClient = originClient;
         this.peerClient = peerClient;
         this.chunkStore = chunkStore;
+        this.peerId = peerId;
+        this.port = port;
     }
 
     public CompletableFuture<TreeSet<SongChunkDto>> start(FilterChunks filter, String songId) {
+
         TreeSet<SongChunkDto> localChunks = filter.getLocalChunks();
         List<SongChunkDto> chunksToFetch = filter.getChunksToFetch();
+
 
         List<CompletableFuture<Void>> futures = chunksToFetch.stream()
                 .map(chunk -> fetchChunk(chunk, songId, localChunks))
@@ -43,16 +46,27 @@ public class Scheduler {
                     Map<String, PeerInfo> chunkPeerMap = peers.get(chunk.getId());
 
                     if (chunkPeerMap == null || chunkPeerMap.isEmpty()) {
-                        return fetchFromOrigin(chunk, songId, localChunks);
+                        return fetchFromOrigin(chunk, songId, localChunks)
+                                .thenCompose(fetched -> {
+                                    if (fetched) {
+                                        return chunkAcquired(new PeerInfo(peerId, "localhost", port), chunk.getId(), songId);
+                                    }
+                                    return CompletableFuture.completedFuture(null);
+                                });
                     }
-
                     return fetchFromPeers(chunk, songId, localChunks, chunkPeerMap)
                             .thenCompose(downloaded -> {
-                                System.out.println("Downloaded: " + downloaded);
-                                if (!downloaded) {
-                                    return fetchFromOrigin(chunk, songId, localChunks);
+                                if (downloaded) {
+                                    return chunkAcquired(new PeerInfo(peerId, "localhost", port), chunk.getId(), songId);
+                                } else {
+                                    return fetchFromOrigin(chunk, songId, localChunks)
+                                            .thenCompose(fetched -> {
+                                                if (fetched) {
+                                                    return chunkAcquired(new PeerInfo(peerId, "localhost", port), chunk.getId(), songId);
+                                                }
+                                                return CompletableFuture.completedFuture(null);
+                                            });
                                 }
-                                return CompletableFuture.completedFuture(null);
                             });
                 })
                 .exceptionally(e -> {
@@ -65,7 +79,6 @@ public class Scheduler {
                                                       TreeSet<SongChunkDto> localChunks,
                                                       Map<String, PeerInfo> chunkPeerMap) {
         CompletableFuture<Boolean> result = CompletableFuture.completedFuture(false);
-
         for (PeerInfo peer : chunkPeerMap.values()) {
             result = result.thenCompose(downloaded -> {
                 if (downloaded) return CompletableFuture.completedFuture(true);
@@ -85,12 +98,16 @@ public class Scheduler {
         peerClient.requestChunk(peer.host(), peer.port(), songId, chunk.getId().toString(), peerFuture::complete);
 
         return peerFuture.thenApply(bytes -> {
-                    if (bytes == null || !validateHash(bytes, chunk.getHash())) return false;
+                    System.out.printf("Successfully requested chunk %s%n", chunk.getId());
 
+                    if (bytes == null || !validateHash(bytes, chunk.getHash())) return false;
                     chunkStore.saveChunk(songId, new Chunk(bytes, chunk.getHash(), chunk.getIndex(), chunk.getId()));
                     synchronized (localChunks) {
                         localChunks.add(new SongChunkDto(chunk.getId(), chunk.getHash(), chunk.getIndex()));
                     }
+
+                    System.out.println("Chunk acquired from " + peer.peerId());
+
                     return true;
                 })
                 .exceptionally(e -> {
@@ -100,18 +117,19 @@ public class Scheduler {
                 });
     }
 
-    private CompletableFuture<Void> fetchFromOrigin(SongChunkDto chunk, String songId,
-                                                    TreeSet<SongChunkDto> localChunks) {
+    private CompletableFuture<Boolean> fetchFromOrigin(SongChunkDto chunk, String songId,
+                                                       TreeSet<SongChunkDto> localChunks) {
         return originClient.fetchChunk(chunk.getId().toString(), songId)
-                .thenAccept(bytes -> {
+                .thenApply(bytes -> {
                     chunkStore.saveChunk(songId, new Chunk(bytes, chunk.getHash(), chunk.getIndex(), chunk.getId()));
                     synchronized (localChunks) {
                         localChunks.add(new SongChunkDto(chunk.getId(), chunk.getHash(), chunk.getIndex()));
                     }
+                    return true;
                 })
                 .exceptionally(e -> {
                     System.err.printf("Origin failed for chunk %s: %s%n", chunk.getId(), e.getMessage());
-                    return null;
+                    return false;
                 });
     }
 
@@ -119,4 +137,28 @@ public class Scheduler {
         // TODO: implement hash validation
         return true;
     }
+
+
+    private CompletableFuture<Void> chunkAcquired(PeerInfo peerInfo, UUID chunkId, String songId) {
+        return trackerClient.register(peerInfo)
+                .thenCompose(registered -> {
+                    System.out.println("Peer with id " + peerInfo.peerId() + " " + registered);
+
+                    return trackerClient.notifyChuckAcquired(new ChunkAcquired(peerId, chunkId, UUID.fromString(songId)))
+                            .thenAccept(notified -> {
+                                System.out.println("Notified chunked acquired " + notified);
+                            })
+
+                            .exceptionally(e -> {
+                                System.err.printf("Failed to notified chunk acquired %s: %s%n", chunkId, e.getMessage());
+                                return null;
+                            });
+
+                })
+                .exceptionally(e -> {
+                    System.err.printf("Failed to notify tracker service of chunk acquired for chunk id %s: and peer id %s: %s%n", chunkId, peerInfo.peerId(), e.getMessage());
+                    return null;
+                });
+    }
+
 }
